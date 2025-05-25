354c6335fc7e6811f77cb266cf8ba53b8d775d6c
ICreateWebApps <denisvejrazka@gmail.com>
2025-05-25 15:41:41 +0200

    Dynamic context

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedNow = now.format(formatter);
+        return "Hello World! Aktuální čas je: " + formattedNow;
+    }
+}
